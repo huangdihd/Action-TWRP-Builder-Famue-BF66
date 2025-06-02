4dd1a263dde6a668b98c292a453ac64905d101ed
huangdihd <74405769+huangdihd@users.noreply.github.com>
2025-06-02 12:26:27 +0800

    Delete vendor/omni directory

--- vendor/omni/common.mk
+++
@@ -1,5 +0,0 @@
-# OmniROM common configuration
-
-# inherit from the common Open Source product configuration
-$(call inherit-product, vendor/omni/config/rom.mk)
-
--- vendor/omni/rom.mk
+++
@@ -1,3 +0,0 @@
-# OmniROM ROM-wide definitions
-PRODUCT_BRAND := omni
-PRODUCT_RESTRICT_VENDOR_FILES := false
